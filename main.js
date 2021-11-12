b16855b580d8c99ca63c088a2c14659808b735b1
Anton Melnychuk <78281795+anton-mel@users.noreply.github.com>
2021-11-12 15:42:04 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,34 @@
+$(document).ready(function(){
 
+  setTimeout(function(){
+    $(".loader h1").animate({opacity: 1}, 2000);
+  }, 500);
+  setTimeout(function(){
+    $(".loader h1").animate({opacity: 0}, 1000);
+  }, 3000);
+  setTimeout(function(){
+    $(".loader").animate({opacity: 0}, 1000);
+    setTimeout(function(){
+      $(".loader").hide();
+    },1000);
+  }, 4000);
 
-$(".tops").click(()=>{
+	$(".tops").click(()=>{
 
-	window.scroll({
-	  top: 0,
-	  left: 0,
-	  behavior: 'smooth'
+		window.scroll({
+		  top: 0,
+		  left: 0,
+		  behavior: 'smooth'
+		});
+
 	});
 
-});
+	$(".menuphone").click(function(){
+		$(".menup").css("transform", "translatex(0)");
+	});
 
-$(".menuphone").click(function(){
-	$(".menup").css("transform", "translatex(0)");
-});
+	$(".clm").click(function(){
+		$(".menup").css("transform", "translatex(100%)");
+	});
 
-$(".clm").click(function(){
-	$(".menup").css("transform", "translatex(100%)");
 });
